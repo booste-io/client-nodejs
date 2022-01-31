11ab687be0ff99d97ab8b1b905d91af4db06ba97
erik-dunteman <erikdunteman@gmail.com>
2022-01-30 16:50:28 -0800

    working ts migration

---
+++ module/index.d.ts
@@ -0,0 +1,3 @@
+export declare function run(apiKey: string, modelKey: string, modelInputs?: object, strategy?: object): Promise<object>;
+export declare function start(apiKey: string, modelKey: string, modelInputs?: object, strategy?: object): Promise<string>;
+export declare function check(apiKey: string, callID: string): Promise<object>;
--- module/index.js
+++ module/index.js
@@ -1,25 +1,34 @@
-const genericsUtils = require("./genericsUtils")
-
-// Generics (calling models by modelKey argument rather than by function name)
-exports.run = async (apiKey, modelKey, modelInputs = {}, strategy = {}) => {
-  const out = await genericsUtils.runMain(
-    apiKey = apiKey, 
-    modelKey = modelKey,
-    modelInputs=modelInputs,
-    strategy = strategy)
-  return out
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.check = exports.start = exports.run = void 0;
+const genericsUtils = require("./genericsUtils");
+function run(apiKey, modelKey, modelInputs = {}, strategy = {}) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const out = yield genericsUtils.runMain(apiKey = apiKey, modelKey = modelKey, modelInputs = modelInputs, strategy = strategy);
+        return out;
+    });
 }
-
-exports.start = async (apiKey, modelKey, modelInputs = {}, strategy= {}) => {
-  const callID = await genericsUtils.startMain(
-    apiKey = apiKey, 
-    modelKey = modelKey,
-    modelInputs=modelInputs,
-    strategy = strategy)
-  return callID
+exports.run = run;
+function start(apiKey, modelKey, modelInputs = {}, strategy = {}) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const callID = yield genericsUtils.startMain(apiKey = apiKey, modelKey = modelKey, modelInputs = modelInputs, strategy = strategy);
+        return callID;
+    });
 }
-
-exports.check = async (apiKey, callID) => {
-  const jsonOut = await genericsUtils.checkMain(apiKey, callID)
-  return jsonOut
+exports.start = start;
+function check(apiKey, callID) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const jsonOut = yield genericsUtils.checkMain(apiKey, callID);
+        return jsonOut;
+    });
 }
+exports.check = check;
